6289d331a19935209ad4ac801ad072ab0fb8cc46
= <d-weatherford@live.com>
2025-11-26 12:03:58 -0800

    implemented secure storage of APIKey

--- AppointmentScheduler/Program.cs
+++ AppointmentScheduler/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using  System.Net.Http;
+using Microsoft.Extensions.Configuration;
+ 
+var sched = new AppointmentScheduler();
+
+public class AppointmentScheduler
+{
+  private string? APIKey { get; init; }
+
+  public AppointmentScheduler()
+  {
+    ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+    IConfiguration config = configBuilder.AddUserSecrets<AppointmentScheduler>().Build();
+    APIKey = config["APIKey"];
+    if (APIKey == null)
+    {
+      throw new Exception("Must Set User Secret 'APIKey'!");
+    }
+  }
+  
+}
